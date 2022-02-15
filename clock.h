8f3cbb03a72299e87c16266fb06e984e9426163b
AgungSatyaMulyana <99741214+AgungSatyaMulyana@users.noreply.github.com>
2022-02-15 21:25:16 +0700

    Add files via upload

---
+++ clock.h
@@ -0,0 +1,28 @@
+#ifndef clock_H
+#define clock_H
+#include "boolean.h"
+#include "stdio.h"
+
+#define Hour(J) (J).HH
+#define Minute(J) (J).MM
+#define Second(J) (J).SS
+typedef struct 
+{
+ int HH; 
+ int MM;
+ int SS;
+} Jam;
+
+Jam MakeJam(int H, int M, int S);
+
+boolean IsJValid(int H, int M, int S);
+
+void BacaJam(Jam *J);
+
+void TulisJam(Jam J);
+
+int JamToDetik(Jam J);
+
+Jam DetikToJam(int N);
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "clock.h"
+
+int main()
+{ 
+   Jam J1, J2;
+   BacaJam(&J1);
+   printf("Jam sekarang: ");
+   TulisJam(J1); printf("\n");
+   printf("Konversi menjadi detik: %d\n",JamToDetik(J1));
+   J2 = DetikToJam(5000);
+   printf("5000 detik sama dengan jam : ");
+   TulisJam(J2);
+   return 0;
+}
